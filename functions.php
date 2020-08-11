91d20ce62cecfb5f1f6773eda9c81b03cf0aa99c
Vasiliy <manilo@ukr.net>
2020-08-11 11:11:07 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -307,6 +307,23 @@ function the_breadcrumb(){
 }
 // breadcrumbs
 
+
+// pagination
+function wp_corenavi() {
+  global $wp_query;
+  $total = isset( $wp_query->max_num_pages ) ? $wp_query->max_num_pages : 1;
+  $a['total'] = $total;
+  $a['mid_size'] = 3; // сколько ссылок показывать слева и справа от текущей
+  $a['end_size'] = 1; // сколько ссылок показывать в начале и в конце
+  $a['prev_text'] = '&laquo;'; // текст ссылки "Предыдущая страница"
+  $a['next_text'] = '&raquo;'; // текст ссылки "Следующая страница"
+
+  if ( $total > 1 ) echo '<nav class="pagination">';
+    echo paginate_links( $a );
+  if ( $total > 1 ) echo '</nav>';
+}
+// pagination
+
 /**
  * Implement the Custom Header feature.
  */
